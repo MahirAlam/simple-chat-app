5246e5ae8355d3c68e642da489c61f628ec410b8
MahirAlam <smmahiralamtohan@gmail.com>
2022-10-04 19:37:08 +0600

    first

---
+++ tailwind.config.cjs
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: ['./index.html', './src/**/*.tsx', './src/**/*.ts'],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('daisyui'),
+  ],
+  daisyui: {
+    themes: [
+      'emerald'
+    ],
+  }
+}
